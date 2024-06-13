ffe42164133cfca37dc766b3aa03881089cae6a6
eleneaa <lenatisenko4@gmail.com>
2024-06-13 22:22:03 +0500

    css

--- book_photoshoot/book/constant.py
+++ book_photoshoot/book/constant.py
@@ -1,7 +1,7 @@
-SIGN_IN_URL = 'http://127.0.0.1:5000/users/authenticate'
-OCCUPIED_BOOK_URL = "http://127.0.0.1:5000/books/occupied"
-CONFIRMED_BOOK_URL = "http://127.0.0.1:5000/books/confirmed"
-LAST_BOOK_URL = "http://127.0.0.1:5000/books/last"
-FREE_BOOK_URL = "http://127.0.0.1:5000/books/free"
-SIGN_UP_URL = "http://127.0.0.1:5000/users/register_user"
-ALL_BOOK_URL = "http://127.0.0.1:5000/books/all"
+SIGN_IN_URL = 'https://elenea.pythonanywhere.com/users/authenticate'
+OCCUPIED_BOOK_URL = "https://elenea.pythonanywhere.com/books/occupied"
+CONFIRMED_BOOK_URL = "https://elenea.pythonanywhere.com/books/confirmed"
+LAST_BOOK_URL = "https://elenea.pythonanywhere.com/books/last"
+FREE_BOOK_URL = "https://elenea.pythonanywhere.com/books/free"
+SIGN_UP_URL = "https://elenea.pythonanywhere.com/users/register_user"
+ALL_BOOK_URL = "https://elenea.pythonanywhere.com/books/all"
